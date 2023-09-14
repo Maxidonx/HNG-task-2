fd2c7e353f1dc88712b0bf4471a3c9770ce46f5a
Maxidonx <johnmaxwell927@gmail.com>
2023-09-14 15:23:46 +0100

    made changes in settings

--- HNG/settings.py
+++ HNG/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d=%)8fe75b4)f!f*&t!b0y1q1=*ani8jl039ygqrmch)nu*7od'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False")==True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'HNG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HNG2',
+        'USER': 'maxwell',
+        'PASSWORD': 'Maxidonx',
     }
 }
-
+database_url= os.environ.get("DATABASE_URL")
+DATABASES ['default']= dj_database_url.parse(database_url)
+#postgres://maxwell_user:Y2ElhzHjJoX3C8pC5fSKvanzfxcCedO3@dpg-ck1gst6ru70s73dnns5g-a.oregon-postgres.render.com/maxwell
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
