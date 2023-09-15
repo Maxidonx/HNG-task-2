70cd30a53f6363171ee493e38a4e6bd363a084af
Maxidonx <johnmaxwell927@gmail.com>
2023-09-15 08:15:19 +0100

    updated serializers

--- peopleAPI/serializers.py
+++ peopleAPI/serializers.py
@@ -5,5 +5,5 @@ class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
-        content_type = ['application/json', 'application/x-www-form-urlencoded']
+    
 
